0021f89ca92b2ab818b47816cc4f11d69f34c18a
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2025-05-10 13:23:01 -0300

    <style>[products]: remove filters

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -45,23 +45,21 @@ export class ProductsComponent implements OnInit {
     })
   }
 
-  searchProduct(idSelected: number): any[] {
-    const product = [];
+  // searchProduct(idSelected: number): any[] {
+  //   const product = [];
 
-    for (let i = 0; i < this.products.length; i++) {
+  //   for (let i = 0; i < this.products.length; i++) {
 
-      if (this.products[i]._id == idSelected) {
-        product.push(this.products[i]);
-      }
-    }
+  //     if (this.products[i]._id == idSelected) {
+  //       product.push(this.products[i]);
+  //     }
+  //   }
 
-    return product
-  }
+  //   return product
+  // }
 
-  modalCreate(idSelected: number | null) {
-    if(idSelected !== null) {
-      const product = this.searchProduct(idSelected);
-
+  modalCreate(product: Product | null) {
+    if(product !== null) {
       this.dialog.open<AddOrEditProductComponent>(AddOrEditProductComponent, {
         width: '70%',
         data: product
@@ -74,14 +72,14 @@ export class ProductsComponent implements OnInit {
     }
   }
 
-  modalDelete(idSelected: number | null) {
-    if (idSelected !== null) {
-      const product = this.searchProduct(idSelected);
-
+  modalDelete(product: Product | null) {
+    if (product !== null) {
       this.dialog.open<DeleteProductComponent>(DeleteProductComponent, {
         width: '70%',
         data: product
       });
+    } else {
+      console.log("[Error]: não foi possível encontrar produto selecionado para excluir");
     }
   }
 
