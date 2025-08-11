a71a2769b13dde2e69eb518b97c6ab6ca32d61b5
Daniel de Souza Cordeiro <Daniel.Cordeiro@ext.rumolog.com>
2025-08-11 13:14:31 -0300

    <feat>[Cart]: Getting Products in Cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -3,6 +3,7 @@ import { UsersService } from '../services/users/users.service';
 import { CartService } from '../services/cart/cart.service';
 import { User } from '../interfaces/user.interface';
 import { Product } from 'src/app/interfaces/product.interface';
+import { P } from '@angular/cdk/portal-directives.d-DbeNrI5D';
 
 @Component({
   selector: 'app-cart',
@@ -12,10 +13,10 @@ import { Product } from 'src/app/interfaces/product.interface';
 })
 export class CartComponent implements OnInit {
   productsInCart: Product[] = [];
+  products: Product[] = [];
   profile: User = {};
 
   constructor(
-    private userService: UsersService,
     private cartService: CartService
   ) {}
 
@@ -23,14 +24,44 @@ export class CartComponent implements OnInit {
     this.gettingProductsInCart();
   }
 
-  gettingProductsInCart(): Product[] {
-    this.gettingUserProfile();
-    return this.productsInCart;
+  gettingProductsInCart(): void {
+    try {
+      const profileUser = this.getUserProfile();
+      const cartProducts = this.getCartProducts() || [];
+
+      if (profileUser.productsCart && profileUser.productsCart.length > 0) {
+        this.productsInCart = [...profileUser.productsCart];
+      } else {
+        console.warn('Nenhum produto encontrado no perfil de usuário(a).');
+      }
+
+      if (cartProducts && cartProducts.length > 0) {
+        for (let index = 0; index < cartProducts.length; index++) {
+          if (!this.productsInCart.some(product => product._id === cartProducts[index]._id)) {
+            this.productsInCart.push(cartProducts[index]);
+          }
+        }
+      } else {
+        console.warn('Nenhum produto encontrado no carrinho local.');
+      }
+    } catch (error) {
+      console.error('Não foi possível obter os produtos no carrinho:', error);
+    }
   }
 
-  gettingUserProfile(): void {
-    const user = localStorage.getItem('profile');
-    this.profile = JSON.parse(user!);
+
+  getUserProfile(): User {
+    const localLoadingUser = localStorage.getItem('profile');
+    this.profile = JSON.parse(localLoadingUser!);
+
+    return this.profile;
+  }
+
+  getCartProducts(): Product[] {
+    const localLoadingProducts = localStorage.getItem('cart');
+    this.products = JSON.parse(localLoadingProducts!);
+
+    return this.products;
   }
 
   cartCleaning(): void {
