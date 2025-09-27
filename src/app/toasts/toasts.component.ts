c13fd83c1a6bd2b8d3b0a432439a8717a240b211
System Administrator <root@MacBook-Pro-de-Camila.local>
2025-09-27 20:15:41 -0300

    <feat>[Toasts]: creat function show toasts

--- src/app/toasts/toasts.component.ts
+++ src/app/toasts/toasts.component.ts
@@ -1,17 +1,76 @@
-import { Component } from '@angular/core';
-import * as bootstrap from 'bootstrap'; // Import Bootstrap JavaScript
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as bootstrap from 'bootstrap';
+
+enum ToastType {
+  SUCCESS = 'success',
+  ERROR = 'error',
+  WARNING = 'warning',
+  INFO = 'info'
+}
+
 
 @Component({
-  selector: 'app-toasts.components',
+  selector: 'app-toasts',
   standalone: false,
   templateUrl: './toasts.component.html',
   styleUrl: './toasts.component.sass'
 })
-export class ToastsComponent {
+export class ToastsComponent implements AfterViewInit{
+  @ViewChild('successToast') successToast!: ElementRef;
+  @ViewChild('errorToast') errorToast!: ElementRef;
+  @ViewChild('warningToast') warningToast!: ElementRef;
+  @ViewChild('infoToast') infoToast!: ElementRef;
 
-  constructor() {
-    const toastElList = document.querySelectorAll('.toast')
-    const toastList = Array.from(toastElList).map(toastEl => new bootstrap.Toast(toastEl, {}))
+  textFromToast: string = 'TESTE DE TOAST SUCESSO';
+
+  ngAfterViewInit() {
+    this.selectedToast('success');
   }
 
+  selectedToast(toastType: string): void {
+    switch (toastType) {
+      case ToastType.SUCCESS:
+        if (this.successToast) {
+          const myToast = new bootstrap.Toast(this.successToast.nativeElement, {
+            autohide: true,
+            delay: 4000
+          });
+
+          myToast.show();
+        }
+        break;
+      case ToastType.ERROR:
+        if (this.errorToast) {
+          const myToast = new bootstrap.Toast(this.errorToast.nativeElement, {
+            autohide: true,
+            delay: 4000
+          });
+
+          myToast.show();
+        }
+        break;
+      case ToastType.WARNING:
+        if (this.warningToast) {
+          const myToast = new bootstrap.Toast(this.warningToast.nativeElement, {
+            autohide: true,
+            delay: 4000
+          });
+
+          myToast.show();
+        }
+        break;
+      case ToastType.INFO:
+        if (this.infoToast) {
+          const myToast = new bootstrap.Toast(this.infoToast.nativeElement, {
+            autohide: true,
+            delay: 4000
+          });
+
+          myToast.show();
+        }
+        break;
+      default:
+        break;
+    }
+  }
 }
