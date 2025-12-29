b4bb06f964807098786872ae45a7bf40652710b3
System Administrator <root@MacBook-Pro-de-Camila.local>
2025-12-29 15:40:41 -0300

    refactor: toast service for recive toast

--- src/app/services/toasts/toasts.service.ts
+++ src/app/services/toasts/toasts.service.ts
@@ -1,72 +1,18 @@
-import { ElementRef, Injectable, ViewChild } from "@angular/core";
-import * as bootstrap from 'bootstrap';
+import { Injectable } from "@angular/core";
+import { Subject } from 'rxjs';
+import { ToastMessage } from '../../interfaces/toast.interface' 
+import { ToastType } from "src/app/enums/toast-type.enum";
 
-enum ToastType {
-    SUCCESS = 'success',
-    ERROR = 'error',
-    WARNING = 'warning',
-    INFO = 'info'
-}
+
 
 @Injectable({
     providedIn: 'root'
 })
 export class ToastsService {
-    typeToast!: string;
-    @ViewChild('successToast') successToast!: ElementRef;
-    @ViewChild('errorToast') errorToast!: ElementRef;
-    @ViewChild('warningToast') warningToast!: ElementRef;
-    @ViewChild('infoToast') infoToast!: ElementRef;
-  
-    textFromToast: string = 'TESTE DE TOAST SUCESSO';
+  private toastSubject = new Subject<ToastMessage>();
+  toast$ = this.toastSubject.asObservable();
 
-    selectedToast(toastType: string): void {
-        switch (toastType) {
-          case ToastType.SUCCESS:
-            if (this.successToast) {
-              const myToast = new bootstrap.Toast(this.successToast.nativeElement, {
-                autohide: true,
-                delay: 4000
-              });
-    
-              myToast.show();
-            }
-            break;
-          case ToastType.ERROR:
-            if (this.errorToast) {
-              const myToast = new bootstrap.Toast(this.errorToast.nativeElement, {
-                autohide: true,
-                delay: 4000
-              });
-    
-              myToast.show();
-            }
-            break;
-          case ToastType.WARNING:
-            console.log('toast de alerta chamado');
-            
-
-            if (this.warningToast) {
-              const myToast = new bootstrap.Toast(this.warningToast.nativeElement, {
-                autohide: true,
-                delay: 4000
-              });
-    
-              myToast.show();
-            }
-            break;
-          case ToastType.INFO:
-            if (this.infoToast) {
-              const myToast = new bootstrap.Toast(this.infoToast.nativeElement, {
-                autohide: true,
-                delay: 4000
-              });
-    
-              myToast.show();
-            }
-            break;
-          default:
-            break;
-        }
-      }
+  show(type: ToastType, message: string): void {
+    this.toastSubject.next({ type, message });
+  }
 }
